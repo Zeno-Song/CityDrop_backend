06584fdb65dca14df66657f3ccf66aff395296d8
Yiyou Liu <161186139+TheUsername1214@users.noreply.github.com>
2026-08-11 04:29:32 -0400

    modify a small mistake

--- src/main/java/com/citydrop/backend/db/OrderRepository.java
+++ src/main/java/com/citydrop/backend/db/OrderRepository.java
@@ -1,8 +1,5 @@
 package com.citydrop.backend.db;
 
-import com.citydrop.backend.db.entities.OrderEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 public interface OrderRepository{
 
 }
--- src/main/java/com/citydrop/backend/db/StationRepository.java
+++ src/main/java/com/citydrop/backend/db/StationRepository.java
@@ -1,7 +1,6 @@
 package com.citydrop.backend.db;
 
 import com.citydrop.backend.db.entities.StationEntity;
-import com.citydrop.backend.db.entities.UserEntity;
 import org.springframework.data.repository.ListCrudRepository;
 
 public interface StationRepository extends ListCrudRepository<StationEntity, Integer> {
