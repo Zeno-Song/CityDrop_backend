809ea205d2a335d053201d28d4a692802f12765a
Yiyou Liu <161186139+TheUsername1214@users.noreply.github.com>
2026-08-11 19:44:52 -0400

    remove unnecessary changes

--- src/main/java/com/citydrop/backend/db/StationRepository.java
+++ src/main/java/com/citydrop/backend/db/StationRepository.java
@@ -1,7 +1,4 @@
 package com.citydrop.backend.db;
 
-import com.citydrop.backend.db.entities.StationEntity;
-import org.springframework.data.repository.ListCrudRepository;
-
-public interface StationRepository extends ListCrudRepository<StationEntity, Integer> {
+public interface StationRepository {
 }
